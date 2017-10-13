8ff5a7779fc0f6bc572bbab65f7803220066c310
WannahHauchope <hwauc0346@westada.org>
2017-10-13 09:54:46 -0600

    Third attempt

--- GitTester/src/GitTest.java
+++ GitTester/src/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
 	public static void main(String[] args) {
 		System.out.println("A Git Test");
 		System.out.println(" a line 2 to demonstrate a change");
+		System.out.println("Another change in order to test Git");
 	}
 }
